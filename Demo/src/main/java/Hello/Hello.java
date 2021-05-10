02e2eb79e445980d5e8bbb25f95e3228a70df715
angela.justiniano <angela.justiniano@avantica.com>
2021-05-09 22:45:33 -0500

    first commit

---
+++ Demo/src/main/java/Hello/Hello.java
@@ -0,0 +1,11 @@
+package Hello;
+
+public class Hello {
+    public String getMessage(boolean bigger) {
+        if (bigger) {
+            return "Hello Universe!";
+        } else {
+            return "Hello World!";
+        }
+    }
+}
